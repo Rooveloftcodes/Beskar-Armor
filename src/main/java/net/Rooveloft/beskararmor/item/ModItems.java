8255a67f7b014deb353a1fab348fc58154b6b8a0
Shaunak Ghosh <98326430+Rooveloftcodes@users.noreply.github.com>
2023-08-30 22:30:54 +0530

    update to 1.19.4

--- src/main/java/net/Rooveloft/beskararmor/item/ModItems.java
+++ src/main/java/net/Rooveloft/beskararmor/item/ModItems.java
@@ -2,7 +2,6 @@ package net.Rooveloft.beskararmor.item;
 
 import net.Rooveloft.beskararmor.Main;
 import net.Rooveloft.beskararmor.item.custom.ModArmorMaterials;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,13 +17,13 @@ public class ModItems
     public static final RegistryObject<Item> RAW_BESKAR_ORE = ITEMS.register("raw_beskar_ore",
             ()-> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BESKAR_HELMET = ITEMS.register("beskar_helmet",
-            () -> new ArmorItem(ModArmorMaterials.BESKAR, EquipmentSlot.HEAD, new Item.Properties()));
+            () -> new ArmorItem(ModArmorMaterials.BESKAR, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> BESKAR_CHESTPIECE = ITEMS.register("beskar_chestpiece",
-            () -> new ArmorItem(ModArmorMaterials.BESKAR, EquipmentSlot.CHEST, new Item.Properties()));
+            () -> new ArmorItem(ModArmorMaterials.BESKAR, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> BESKAR_LEGGINGS = ITEMS.register("beskar_leggings",
-            () -> new ArmorItem(ModArmorMaterials.BESKAR, EquipmentSlot.LEGS, new Item.Properties()));
+            () -> new ArmorItem(ModArmorMaterials.BESKAR, ArmorItem.Type.LEGGINGS, new Item.Properties()));
     public static final RegistryObject<Item> BESKAR_BOOTS = ITEMS.register("beskar_boots",
-            () -> new ArmorItem(ModArmorMaterials.BESKAR, EquipmentSlot.FEET, new Item.Properties()));
+            () -> new ArmorItem(ModArmorMaterials.BESKAR, ArmorItem.Type.BOOTS, new Item.Properties()));
     public static void register(IEventBus eventBus)
     {
         ITEMS.register(eventBus);
